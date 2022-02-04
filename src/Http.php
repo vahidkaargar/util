4d7876f42b90fcb0d678ddae8483866026546b4f
Vahid Kaargar <vahidkaargar@gmail.com>
2022-02-04 21:49:55 +0330

    Create Http.php

---
+++ src/Http.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace vahidkaargar\util;
+
+use GuzzleHttp\Client;
+
+
+class Http
+{
+    private static $instance = null;
+    private $client;
+
+    public static function factory()
+    {
+        if (is_null(self::$instance))
+            self::$instance = new Http();
+        return self::$instance;
+    }
+
+    public function client()
+    {
+        $this->client = new Client();
+    }
+
+    public function request($method, $url, $options = [])
+    {
+        $request = $this->client->request('GET', $url, $options);
+        $response = [
+            'success' => false,
+            'message' => 'Request status code is not 200'
+        ];
+        if ($request->getStatusCode() === 200)
+            $response = Json::decode($request->getBody()->getContents());
+        return $response;
+    }
+}
